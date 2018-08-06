49b22eb1b69bb6a382b4e5faf3b600a05c9c10b7
mvind <vms754@protonmail.com>
2018-08-06 18:49:33 +0200

    finished partway_permutation.

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -8,7 +8,7 @@ def get_permutations(sequence):
     Enumerate all permutations of a given string
 
     sequence (string): an arbitrary string to permute. Assume that it is a
-    non-empty string.  
+    non-empty string.
 
     You MUST use recursion for this part. Non-recursive solutions will not be
     accepted.
@@ -23,7 +23,41 @@ def get_permutations(sequence):
     a different order than what is listed here.
     '''
 
-    pass #delete this line and replace with your code here
+    if len(sequence) == 1:
+        return list(sequence)
+
+#   abc
+#   ab ba
+#   cab acb abc cba bca bac
+#
+
+    permutations = []
+
+    #init
+    slice1 = sequence[0:2]
+    slice2 = slice1[-1]+slice1[0]
+    print(slice1, slice2)
+
+
+def partway_permuation(slice,c):
+    "Returns all permuations by inserting c into slice"
+    permutations = []
+    li = list(slice)
+
+    #First case
+    permutations.append([c]+li)
+
+    #Rest of cases
+    for i in range(1,len(slice)+1):
+        #print(li[0:i]+[c]+li[i:])
+        permutations.append(li[0:i]+[c]+li[i:])
+    print(permutations)
+    #print(li)
+
+
+
+
+partway_permuation('abc','d')
 
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -31,10 +65,9 @@ if __name__ == '__main__':
 #    print('Input:', example_input)
 #    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
 #    print('Actual Output:', get_permutations(example_input))
-    
+
 #    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
-#    sequence of length n)
-
+#    to be three characters or fewer as you will have n! permutations for a
+    #    sequence of length n)
+    #get_permutations('abc')
     pass #delete this line and replace with your code here
-
